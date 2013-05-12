koraktor/mestral

commit c82c6ff3ad893db85358c6596016559029ddf725

Fix source installation repository

install.rb
@@ -41,7 +41,7 @@ unless executable? 'git'
   exit
 end
 
-MESTRAL_REPOSITORY = 'https://github.com/koraktor/mestral'
+MESTRAL_REPOSITORY = ENV["MESTRAL_REPOSITORY] || 'https://github.com/mestral/mestral'
 
 if windows?
   MESTRAL_PREFIX = "#{ENV['ALLUSERSPROFILE']}/Mestral"

commit 70ee3ee64f7f027cbc2dcc52b4bf9fd271b0d1a0

Fix syntax error introduced in c82c6ff3ad893db85358c6596016559029ddf725
Thanks to @johnallen3d.

install.rb
@@ -41,7 +41,7 @@ unless executable? 'git'
   exit
 end
 
-MESTRAL_REPOSITORY = ENV["MESTRAL_REPOSITORY] || 'https://github.com/mestral/mestral'
+MESTRAL_REPOSITORY = ENV["MESTRAL_REPOSITORY"] || 'https://github.com/mestral/mestral'
 
 if windows?
   MESTRAL_PREFIX = "#{ENV['ALLUSERSPROFILE']}/Mestral"